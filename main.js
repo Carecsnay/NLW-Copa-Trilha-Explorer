fa3be54b11c66f5dbd98a634bb098a45219fed00
Bruno Benicio de Andrade Lima <carecsnay1@gmail.com>
2022-11-06 00:08:44 -0300

    A4 20:00

--- main.js
+++ main.js
@@ -7,14 +7,17 @@ function createGame(player1, hour, player2) {
     </li>
     `
 }
+// let delay = -0.3;
 
-function createCard(date, day, games) {
+function createCard(date, day, game) {
+    // delay = delay + 0.3;
+    // style="animation-delay: ${delay}s"
     return `
-        <div class="card">
+        <div class="card"> 
             <h2>${date}<span>${day}</span></h2>
 
             <ul>
-                ${games}
+                ${game}
             </ul>
         </div> 
     `
@@ -22,33 +25,27 @@ function createCard(date, day, games) {
 
 document.querySelector('#cards').innerHTML = 
         
-          createCard("24/11", "Quinta", 
+        createCard("24/11", "Quinta", 
           
-          createGame("Switzerland", "07", "Cameroon")
-        + createGame("Uruguay", "10", "South Korea")  /* Pra add jogos basta concatenar "+" depois de um createGame */ 
+        createGame("Switzerland", "07", "Cameroon")     // Pra add jogos basta concatenar "+" depois de um createGame 
+        + createGame("Uruguay", "10", "South Korea") 
         + createGame("Portugal", "13", "Ghana")
-        + createGame("Brazil", "16", "Serbia") 
-
-        )
+        + createGame("Brazil", "16", "Serbia"))
     
-        + //cria um card
+        + //cria + um card
         
         createCard("28/11", "Segunda", 
         
-          createGame("Cameroon", "07", "Serbia")
+        createGame("Cameroon", "07", "Serbia")
         + createGame("South Korea", "10", "Ghana")
         + createGame("Brazil", "13", "Switzerland")
-        + createGame("Uruguay", "16", "Portugal")
-
-        )
+        + createGame("Uruguay", "16", "Portugal"))
     
-        + //cria um card
+        + //cria + um card
         
         createCard("02/12", "Sexta", 
         
-          createGame("South Korea", "12", "Portugal")
+        createGame("South Korea", "12", "Portugal")
         + createGame("Ghana", "12", "Uruguay")
         + createGame("Serbia", "16", "Switzerland")
-        + createGame("Brazil", "16", "Cameroon")
-        
-        )
+        + createGame("Brazil", "16", "Cameroon"))
