2f527191e660df69d1d1ebe0db042a2454e74fc6
Bruno Benicio de Andrade Lima <carecsnay1@gmail.com>
2022-11-05 23:26:46 -0300

    A03-C

---
+++ main.js
@@ -0,0 +1,61 @@
+function createGame(player1, hour, player2) {
+    return `
+    <li>
+        <img src="./assets/flags/icon-${player1}.svg" alt="Bandeira do ${player1}">
+            <strong>${hour}:00</strong>
+        <img src="./assets/flags/icon-${player2}.svg" alt="Bandeira da ${player2}">
+    </li>
+    `
+}
+
+function createCard(date, day, games) {
+    return `
+        <div class="card">
+            <h2>${date}<span>${day}</span></h2>
+
+            <ul>
+                ${games}
+            </ul>
+        </div> 
+    `
+}
+
+document.querySelector('#app').innerHTML = `
+    <header>
+        <img src="./assets/logo.svg" alt="logo">
+    </header>
+
+    <main id="cards">
+        ${
+          createCard("24/11", "Quinta", 
+          
+          createGame("Switzerland", "07", "Cameroon")
+        + createGame("Uruguay", "10", "South Korea")  /* Pra add jogos basta concatenar "+"depois de um jogo */ 
+        + createGame("Portugal", "13", "Ghana")
+        + createGame("Brazil", "16", "Serbia") 
+        
+        )
+    }
+        
+        
+        ${createCard("28/11", "Segunda", 
+        
+          createGame("Cameroon", "07", "Serbia")
+        + createGame("South Korea", "10", "Ghana")
+        + createGame("Brazil", "13", "Switzerland")
+        + createGame("Uruguay", "16", "Portugal")
+        )
+    }
+        
+        
+        ${createCard("02/12", "Sexta", 
+        
+          createGame("South Korea", "12", "Portugal")
+        + createGame("Ghana", "12", "Uruguay")
+        + createGame("Serbia", "16", "Switzerland")
+        + createGame("Brazil", "16", "Cameroon")
+               
+        )
+    }
+    </main>  
+`
